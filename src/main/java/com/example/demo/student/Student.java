7c04a3240c495ffd86e0ff553868dbaf14731323
alexSam <alexsamalot19@gmail.com>
2022-05-10 12:36:41 -0400

    Setup local Postgres Docker container for testing (similar to legacy version). PreDeployment changes

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,6 +1,9 @@
 package com.example.demo.student;
 
 import lombok.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
 
 @ToString
 @Getter
@@ -8,10 +11,22 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator="student_sequence",
+            strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
 }
